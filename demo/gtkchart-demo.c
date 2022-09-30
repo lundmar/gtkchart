95b12a2b275332bd4fa722f94042143057ab103e
Martin Lund <martin.lund@keep-it-simple.com>
2022-09-30 15:56:11 +0200

    Use libadwaita
    To make sure that color themes are updated consistently.

--- demo/gtkchart-demo.c
+++ demo/gtkchart-demo.c
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <gtk/gtk.h>
 #include <gtkchart.h>
+#include <adwaita.h>
 
 static GtkChart *chart;
 static GMutex producer_mutex;
@@ -26,14 +27,14 @@ static gboolean gui_chart_plot_thread(gpointer user_data)
     return G_SOURCE_REMOVE;
 }
 
-static void app_activate(GApplication *app, gpointer user_data)
+static void activate_cb(GApplication *app, gpointer user_data)
 {
     GtkWidget *window;
     (void)user_data;
 
-    window = gtk_application_window_new(GTK_APPLICATION (app));
+    window = gtk_application_window_new(GTK_APPLICATION(app));
     gtk_window_set_title(GTK_WINDOW(window), "gtkchart-demo");
-    gtk_window_set_default_size(GTK_WINDOW (window), 1000, 700);
+    gtk_window_set_default_size(GTK_WINDOW(window), 1000, 700);
 
     chart = GTK_CHART(gtk_chart_new());
     gtk_chart_set_type(chart, GTK_CHART_TYPE_LINE);
@@ -45,7 +46,7 @@ static void app_activate(GApplication *app, gpointer user_data)
     gtk_chart_set_y_max(chart, 10);
     gtk_chart_set_width(chart, 800);
 
-    gtk_window_set_child(GTK_WINDOW (window), GTK_WIDGET(chart));
+    gtk_window_set_child(GTK_WINDOW(window), GTK_WIDGET(chart));
 
     gtk_widget_show(window);
 
@@ -82,16 +83,16 @@ static gpointer producer_function(gpointer user_data)
 
 int main(int argc, char **argv)
 {
-    GtkApplication *app;
     int stat;
+    g_autoptr (AdwApplication) app = NULL;
 
     // Spawn data generating thread
     g_mutex_lock(&producer_mutex);
     GThread *producer_thread = g_thread_new("producer thread", producer_function, NULL);
 
-    // Start GTK application
-    app = gtk_application_new("com.github.lundmar.gtkchart", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK (app_activate), NULL);
+    // Start GTK/Adwaita application
+    app = adw_application_new("com.github.lundmar.gtkchart", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK (activate_cb), NULL);
     stat = g_application_run(G_APPLICATION (app), argc, argv);
 
     // Clean up
